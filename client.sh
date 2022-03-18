409841294f7a3e2df0977c3a30742e55f100ffaf
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-03-18 15:16:52 +0100

    feat: test different iperf3 settings

--- client.sh
+++ client.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-iperf3 -u -c 10.0.0.2 -b 1000m -V -J > network-test.log
+for i in {1..20}
+echo '[ \n' > network-test.log
+do
+    iperf3 -u -c 10.0.0.2 -b ${i}00m -V -J > network-test.log
+    echo ', \n' > network-test.log
+done
+echo '] \n' > network-test.log
