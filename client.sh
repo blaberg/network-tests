c4f514c65951365aaf3776ae95cef4fa536d565e
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-03-18 15:22:37 +0100

    fix: interpret \n as newline

--- client.sh
+++ client.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo '[ \n' > network-test.log
+echo -e '[ \n' > network-test.log
 for i in {1..20}
 do
+    echo "Test $i"
     iperf3 -u -c 10.0.0.2 -b ${i}00m -V -J > network-test.log
     echo ', \n' >> network-test.log
 done
-echo '] \n' >> network-test.log
+echo -e '] \n' >> network-test.log
