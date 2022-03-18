82ca95c7a3b0e9f093f51e302e96728bcc52b280
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-03-18 15:20:15 +0100

    fix: append text to file

--- client.sh
+++ client.sh
@@ -3,6 +3,6 @@ echo '[ \n' > network-test.log
 for i in {1..20}
 do
     iperf3 -u -c 10.0.0.2 -b ${i}00m -V -J > network-test.log
-    echo ', \n' > network-test.log
+    echo ', \n' >> network-test.log
 done
-echo '] \n' > network-test.log
+echo '] \n' >> network-test.log
