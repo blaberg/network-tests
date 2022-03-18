841c978f1119e914bd0be90797fe08cc40637637
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-03-18 16:24:38 +0100

    fix: syntax error

--- client.sh
+++ client.sh
@@ -4,6 +4,6 @@ for i in 2 4 6 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
     echo "Test $i"
     iperf3 -u -c 10.0.0.2 -b ${i}00m -J >> network-test.json
-    echo ', \n' >> network-test.log
+    echo ', \n' >> network-test.json
 done
 echo '] \n' >> network-test.json
