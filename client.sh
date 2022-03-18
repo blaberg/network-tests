ffa2b3f977ac2fe96768c73f5d4e9fc0b98b3f31
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-03-18 15:17:47 +0100

    fix: syntex error

--- client.sh
+++ client.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-for i in {1..20}
 echo '[ \n' > network-test.log
+for i in {1..20}
 do
     iperf3 -u -c 10.0.0.2 -b ${i}00m -V -J > network-test.log
     echo ', \n' > network-test.log
