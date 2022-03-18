8563267ec186ddc73572abc1d56b5579778a2973
John Blåberg Kristoffersson <John.kristoffersson@gmail.com>
2022-03-18 16:37:08 +0100

    fix: add more bitrates

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo '[ \n' > network-test.json
-for i in 2 4 6 8 9 10 11 12 13 14 15 16 17 18 19 20
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
     echo "Test $i"
     iperf3 -u -c 10.0.0.2 -b ${i}00m -J >> network-test.json
